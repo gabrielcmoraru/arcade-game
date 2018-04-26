093065448811f3f02d80a434781cd337d5f10277
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-04-26 23:11:41 +0100

    removed star image; github is bad with uppercase pictures

--- js/engine.js
+++ js/engine.js
@@ -177,8 +177,7 @@ var Engine = (function(global) {
         'images/char-cat-girl.png',
         'images/char-horn-girl.png',
         'images/char-pink-girl.png',
-        'images/char-princess-girl.png',
-        'images/star.png'
+        'images/char-princess-girl.png'
     ]);
     Resources.onReady(init);
 
